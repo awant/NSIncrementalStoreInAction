9896619a6e8f209125405d28e70144aa635796b2
Admin <a467bot@icloud.com>
2015-10-12 22:17:19 +0300

    added carthage support.

---
+++ ResultsFetcher/Job+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  Job+CoreDataProperties.swift
+//  ResultsFetcher
+//
+//  Created by Admin on 23.09.15.
+//  Copyright © 2015 Artemiy Sobolev. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Job {
+
+    @NSManaged var name: String?
+    @NSManaged var persons: NSMutableSet?
+
+}
